a9236b64dfadb75ed0b6292d74e151332019cd3e
Tyler <rhinehart22@gmail.com>
2017-08-11 10:47:15 -0600

    displays weather temp in Kelvin and quote

--- public/app/components/quote/quote-controller.js
+++ public/app/components/quote/quote-controller.js
@@ -2,7 +2,19 @@ function QuoteController(){
 
 	var qs = new QuoteService()
 
-	qs.getQuote(function(quote){
-		console.log('What is the quote', quote)
-	})
+	function getQuote() {
+		qs.getQuote(drawQuote)
+	}
+
+	// qs.getQuote(function(quote){
+	// 	console.log('What is the quote', quote)
+	// })
+
+	function drawQuote(quoteObj) {
+		console.log(quoteObj)
+		var quote = quoteObj.quote
+		document.getElementById('quote').innerHTML = quote
+	}
+
+	getQuote()
 }
--- public/app/components/weather/weather-controller.js
+++ public/app/components/weather/weather-controller.js
@@ -1,10 +1,23 @@
-function WeatherController(){
+function WeatherController() {
 	var wc = this;
 	var weatherService = new WeatherService();
-	
-	weatherService.getWeather(function(weather){
-		console.log(weather);
-		//What can you do with this weather object?
-	})
+
+	weatherService.getWeather(drawWeather)
+	//What can you do with this weather object?
+
+
+	function drawWeather(obj) {
+		console.log(obj.main)
+		var template = ''
+		template = `
+			<div class="row">
+				<div class="col-xs-1 col-xs-offset-11">
+					<h3>${obj.name}</h3>
+					<h4>${obj.main.temp} K</h4>
+				</div>
+			</div>
+		`
+		document.getElementById('weather').innerHTML = template
+	}
 
 }
