1a5de04219afb57c585e1f70e61cf392eefb35b8
Tyler <rhinehart22@gmail.com>
2017-08-11 10:25:05 -0600

    background from api is added to screen

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 //require your dependencies
+var express = require('express')
+var bodyParser = require('body-parser')
+var dbConnect = require('./config/db/mlab-config')
+var server = express()
+var port = 3000
 
 
 
 //tell your server what it needs to use
-
-
+server.use(express.static(__dirname + '/public'))
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
 
 //register your todos routes as api/routes
 
 
 
 //start your server listening....
+server.listen(port, () => {
+
+    console.log(`
+        Starting up node,
+        Available on:
+        http://127.0.0.1:${port}
+        Hit CTRL-C to stop the server`)
+
+})
--- public/app/components/image/image-controller.js
+++ public/app/components/image/image-controller.js
@@ -1,5 +1,19 @@
 function ImageController() {
 	//Your ImageService is a global constructor function what can you do here if you new it up?
+	var imageService = new ImageService()
+
+	function getImage() {
+		imageService.getImage(drawImage)
+	}
+
+	function drawImage(image) {
+		console.log(image)
+		var url = image.large_url
+		console.log(url)
+		document.body.style.background = `url(${url}) no-repeat center center fixed`
+	}
+
+	getImage()
 
 }
 
